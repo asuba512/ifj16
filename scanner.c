d0e33bf57ae8a9ffef62fe2eb1e934d4bafda20b
Adam Suba <asuba512@gmail.com>
2016-10-05 12:12:00 +0200

    Scanner now reads string and ignores comments.

--- scanner.c
+++ scanner.c
@@ -6,12 +6,15 @@
 
 #include "scanner.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 int get_token(FILE *fd) {
 
-	int c, i = 0;
+	int c, i = 0, o = 0;
 	char buff[100]; // temporary
+	char octal[4];
+	char *endptr;
 	
 	t_state state = state_init;
 
@@ -23,7 +26,9 @@ int get_token(FILE *fd) {
 
         switch(state){
             case state_init:
-                if(isalpha(c) || c == '$' || c == '_'){
+                if(c == '/')
+					state = _state_division;
+                else if(isalpha(c) || c == '$' || c == '_'){
 					buff[i++] = c;
                     state = state_identifier;
                 }
@@ -32,9 +37,36 @@ int get_token(FILE *fd) {
 					buff[i++] = c;
 					state = state_integer;
 				}
+				else if(c == '"'){
+					buff[i++] = c;
+					state = state_string;
+				}
 					
                 break;
-
+			case _state_division:
+				if(c == '/')
+					state  = state_linecomment;
+				else if(c == '*')
+					state = _state_blockcomment;
+				else{ // <- future division sign return
+					ungetc(c, fd);
+					state = state_init;
+				}
+				break;
+			case state_linecomment:
+				if(c == '\n')
+					state = state_init;
+				break;
+			case _state_blockcomment:
+				if(c == '*')
+					state = state_blockcomment;
+				break;
+			case state_blockcomment:
+				if(c == '/')
+					state = state_init;
+				else
+					state = _state_blockcomment;
+				break;
             case state_identifier:
                 if(isalnum(c) || c == '$' || c == '_')
 					buff[i++] = c;
@@ -72,7 +104,7 @@ int get_token(FILE *fd) {
 					buff[i++] = c;
 					state = state_double;
 				}
-				else{
+				else{ // <- future error state
 					ungetc(c, fd);
 					i = 0;
 					state = state_init;
@@ -103,7 +135,7 @@ int get_token(FILE *fd) {
 					buff[i++] = c;
 					state = _state_double_e_sign;
 				}
-				else{
+				else{ // <- future error state
 					ungetc(c, fd);
 					i = 0;
 					state = state_init;
@@ -114,13 +146,13 @@ int get_token(FILE *fd) {
 					buff[i++] = c;
 					state = state_double_e;
 				}
-				else{
+				else{ // <- future error state
 					ungetc(c, fd);
 					i = 0;
 					state = state_init;
 				}
 				break;
-			case state_double_e:
+case state_double_e:
 				if(isdigit(c))
 					buff[i++] = c;
 				else{
@@ -131,6 +163,68 @@ int get_token(FILE *fd) {
 					state = state_init;
 				}				
 				break;
+			case state_string:
+				if(c == '\\'){
+					state = _state_string_escape;
+				}
+				else if(c == '"'){
+					buff[i++] = c;
+					buff[i] = 0;
+					printf("STRING: %s\n", buff);
+					i = 0;
+					state = state_init;
+				}
+				else
+					buff[i++] = c;
+				break;
+			case _state_string_escape:
+				if(c == 't' || c == 'n' || c == '"' || c == '\\'){
+					switch(c){
+						case 't':
+							buff[i++] = '\t'; break;
+						case 'n':
+							buff[i++] = '\n'; break;
+						case '"':
+							buff[i++] = '\"'; break;
+						case '\\':
+							buff[i++] = '\\'; break;
+					}
+					state = state_string;
+				}
+				else if(c >= '0' && c <= '3'){
+					octal[o++] = c;
+					state = _state_string_octalx;
+				}
+				else{ // <- future error state
+					ungetc(c, fd);
+					i = 0;
+					state = state_init;
+				}
+				break;
+			case _state_string_octalx:
+				if(c >= '0' && c <= '7'){
+					octal[o++] = c;
+					state = _state_string_octalxx;
+				}
+				else{ // <- future error state
+					ungetc(c, fd);
+					i = 0;
+					state = state_init;
+				}
+				break;
+			case _state_string_octalxx:
+				if(c >= '0' && c <= '7'){
+					octal[o++] = c;
+					octal[o] = 0;
+					buff[i++] = strtol(octal, &endptr, 8);
+					o = 0;
+					state = state_string;
+				}
+				else{ // <- future error state
+					ungetc(c, fd);
+					i = 0;
+					state = state_init;
+				}
         }
 
     }
--- scanner.h
+++ scanner.h
@@ -14,6 +14,10 @@
 
 typedef enum {
 	state_init,
+	_state_division,
+	_state_blockcomment,
+	state_blockcomment,
+	state_linecomment,
 	state_identifier,
 	state_integer,
 	_state_double_point,
@@ -21,6 +25,10 @@ typedef enum {
 	_state_double_e,
 	_state_double_e_sign,
 	state_double_e,
+	state_string,
+	_state_string_escape,
+	_state_string_octalx,
+	_state_string_octalxx
 } t_state;
 
 int get_token(FILE *);
