281c08c8204c5809c62a9033194b4ee4a458b25f
europ <adricko28@gmail.com>
2016-10-14 19:22:20 +0200

    BMA - pascal to c, not done yet

--- ial.c
+++ ial.c
@@ -7,111 +7,142 @@
 
 static bst_node_t _bst_helper_ptr;
 
+/*
+void BMA_compute_jumps(string_t s, int char_jump[][]) {
+    for(int i=0; i<255; i++) {
+        char_jumps[i]=s->length;
+    }
+    for(int j=0; j<s->length; j++0) {
+        char_jumps[(int)s->data][j] = s->length-j
+    }
+    return;
+}
+
+int BMA(string_t s, string_t substring, int char_jump[][], int match_jump[][]) {
+    int a,b,max;
+    j = s->length;
+    k = s->length;
+    while (j<=substring->length && k>0) {
+        if (substring->data[j] == s->data[k]) {
+            j = j-1;
+            k = k-1;
+        }
+        else {
+            if () max = ;
+            else max = ;
+            j = j+max;
+            k = substring->length;
+        }
+    }
+    return (k == 0) ? 0 : -1; // (0 = substring found), (-1 = substring not found)
+}
+*/
+
 // Algorithm from "Opora-IAL-2014-verze-15-A.pdf" (page 156)
 void shell_sort(string_t s) {
-	char tmp;
-	int i,j;
-	int step = s->length / 2;
-	while (step > 0) {
-		for(i=step; i<=((s->length)-1); i++) {
-			j=i-step;
-			while ((j>=0) && (s->data[j]>s->data[j+step])) {
-				tmp = s->data[j];				// ====
-				s->data[j] = s->data[j+step];	// SWAP
-				s->data[j+step] = tmp;			// ====
-				j -= step;
-			}
-		}
-		step = step/2;
-	}
-	return;
+    char tmp;
+    int i,j;
+    int step = s->length / 2;
+    while (step > 0) {
+        for(i=step; i<=((s->length)-1); i++) {
+            j=i-step;
+            while ((j>=0) && (s->data[j]>s->data[j+step])) {
+                tmp = s->data[j];               // ====
+                s->data[j] = s->data[j+step];   // SWAP
+                s->data[j+step] = tmp;          // ====
+                j -= step;
+            }
+        }
+        step = step/2;
+    }
+    return;
 }
 
 bool bst_search(bst_node_t root, string_t key) {
-	if(root != NULL) {
-		if(str_compare(root->key, key) == 0) {
-			return true;
-		} else {
-			if(str_compare(root->key, key) > 0) {
-				return bst_search(root->left_p, key);
-			} else {
-				return bst_search(root->right_p, key);
-			}
-		}
-	} else {
-		return false;
-	}
+    if(root != NULL) {
+        if(str_compare(root->key, key) == 0) {
+            return true;
+        } else {
+            if(str_compare(root->key, key) > 0) {
+                return bst_search(root->left_p, key);
+            } else {
+                return bst_search(root->right_p, key);
+            }
+        }
+    } else {
+        return false;
+    }
 }
 
 bst_node_t bst_search_get(bst_node_t root, string_t key) {
-	if(root == NULL) {
-		return NULL;
-	} else {
-		if(str_compare(root->key, key) != 0) {
-			if(str_compare(root->key, key) > 0) {
-				return bst_search_get(root->left_p, key);
-			} else {
-				return bst_search_get(root->right_p, key);
-			}
-		} else {
-			return root;
-		}
-	}
+    if(root == NULL) {
+        return NULL;
+    } else {
+        if(str_compare(root->key, key) != 0) {
+            if(str_compare(root->key, key) > 0) {
+                return bst_search_get(root->left_p, key);
+            } else {
+                return bst_search_get(root->right_p, key);
+            }
+        } else {
+            return root;
+        }
+    }
 }
 
 int bst_insert(bst_node_t *root_ptr, string_t key, void *data) {
-	if(*root_ptr == NULL) {
-		return _bst_create_node(root_ptr, key, data);
-	} else {
-		if(str_compare((*root_ptr)->key, key) > 0) {
-			return bst_insert(&((*root_ptr)->left_p), key, data);
-		} else if(str_compare((*root_ptr)->key, key) < 0) {
-			return bst_insert(&((*root_ptr)->right_p), key, data);
-		} else {
-			(*root_ptr)->data = data;
-			return 0;
-		}
-	}
+    if(*root_ptr == NULL) {
+        return _bst_create_node(root_ptr, key, data);
+    } else {
+        if(str_compare((*root_ptr)->key, key) > 0) {
+            return bst_insert(&((*root_ptr)->left_p), key, data);
+        } else if(str_compare((*root_ptr)->key, key) < 0) {
+            return bst_insert(&((*root_ptr)->right_p), key, data);
+        } else {
+            (*root_ptr)->data = data;
+            return 0;
+        }
+    }
 }
 
 int _bst_create_node(bst_node_t *node_ptr, string_t key, void *data) {
-	*node_ptr = malloc(sizeof(struct bst_node));
-	if(*node_ptr == NULL) return BST_ERR;
-	(*node_ptr)->left_p = (*node_ptr)->right_p = NULL;
-	(*node_ptr)->key = key;
-	(*node_ptr)->data = data;
-	return 0;
+    *node_ptr = malloc(sizeof(struct bst_node));
+    if(*node_ptr == NULL) return BST_ERR;
+    (*node_ptr)->left_p = (*node_ptr)->right_p = NULL;
+    (*node_ptr)->key = key;
+    (*node_ptr)->data = data;
+    return 0;
 }
 
 void bst_delete(bst_node_t *root, string_t key) {
-	if(*root != NULL) {
-		if(str_compare((*root)->key, key) > 0) {
-			bst_delete(&((*root)->left_p), key);
-		} else if(str_compare((*root)->key, key) < 0) {
-			bst_delete(&((*root)->right_p), key);
-		} else {
-			_bst_helper_ptr = *root;
-			if(_bst_helper_ptr->right_p == NULL) {
-				*root = _bst_helper_ptr->left_p;
-			} else if(_bst_helper_ptr->left_p == NULL) {
-				*root = _bst_helper_ptr->right_p;
-			} else {
-				_bst_del(&(_bst_helper_ptr->left_p));
-			}
-			str_destroy(_bst_helper_ptr->key);
-			free(_bst_helper_ptr);
-			_bst_helper_ptr = NULL;
-		}
-	}
+    if(*root != NULL) {
+        if(str_compare((*root)->key, key) > 0) {
+            bst_delete(&((*root)->left_p), key);
+        } else if(str_compare((*root)->key, key) < 0) {
+            bst_delete(&((*root)->right_p), key);
+        } else {
+            _bst_helper_ptr = *root;
+            if(_bst_helper_ptr->right_p == NULL) {
+                *root = _bst_helper_ptr->left_p;
+            } else if(_bst_helper_ptr->left_p == NULL) {
+                *root = _bst_helper_ptr->right_p;
+            } else {
+                _bst_del(&(_bst_helper_ptr->left_p));
+            }
+            str_destroy(_bst_helper_ptr->key);
+            free(_bst_helper_ptr);
+            _bst_helper_ptr = NULL;
+        }
+    }
 }
 
 void _bst_del(bst_node_t *node) {
-	if((*node)->right_p != NULL) {
-		_bst_del(&((*node)->right_p));
-	} else {
-		_bst_helper_ptr->data = (*node)->data;
-		_bst_helper_ptr->key = (*node)->key;
-		_bst_helper_ptr = *node;
-		*node = (*node)->left_p;
-	}
+    if((*node)->right_p != NULL) {
+        _bst_del(&((*node)->right_p));
+    } else {
+        _bst_helper_ptr->data = (*node)->data;
+        _bst_helper_ptr->key = (*node)->key;
+        _bst_helper_ptr = *node;
+        *node = (*node)->left_p;
+    }
 }
