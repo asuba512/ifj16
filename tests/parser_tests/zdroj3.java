d7396c64c9ac1853fff4df2d1e692942ef05cbdc
xpalie00 <xpalie00@stud.fit.vutbr.cz>
2016-11-11 14:52:51 +0100

    zdroj3.java

---
+++ tests/parser_tests/zdroj3.java
@@ -0,0 +1,56 @@
+/* Program 3: Prace s retezci a vestavenymi funkcemi */
+class Main
+{
+    static int x;
+    static void run()
+    {
+        x = 5 + 5 * 5 - 5 / 5;
+        String str1;
+        str1 = "Toto je nejaky text";
+        String str2;
+        str2 = str1 + ", ktery jeste trochu obohatime";
+        Main.x = ifj16.find(str2, "text");
+        x = 6;
+        Game.play(str1);
+    }
+
+    static String test1(int a, boolean bool, int c, String str) {
+        
+    }
+    static boolean test2(int a, boolean bool, int c, String str) {
+        
+    }
+// end of static void run()
+}
+// end of class Main
+class Game
+{
+    static double test1(int a, boolean bool, int c, String str) {
+        
+    }
+
+    static boolean test2(int a, boolean bool, double dbl, int c, String str, boolean dbool, double dgsbl, int cga, String sgtr, boolean dgabl) {
+        
+    }
+
+    static void play(String str)
+    {
+        Main.run();
+        main.test1();
+        a = 2;
+        double d = 42;
+        int cmp = 42;
+        if (cmp != 0) {
+            while (cmp != 0) {
+                a = 4+4;
+            }
+        }
+        else {
+        }
+        boolean f;
+        double d;
+        return;
+    }
+    // end of static void play(String)
+}
+// end of class Game
