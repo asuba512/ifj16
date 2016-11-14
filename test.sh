277836f7cba240a436ac289f80574cbea7b3c8ab
Adrián Tóth <adricko28@gmail.com>
2016-11-14 16:08:51 +0100

    test.sh, output is printed when something isnt OK

--- test.sh
+++ test.sh
@@ -5,38 +5,43 @@ GOOD_dir="`pwd`/tests/codes_without_error"
 BAD_dir="`pwd`/tests/semantic_tests"
 
 ############################################################
-printf "=== IFJ BUILD ===\n"
+printf "\n=== IFJ BUILD ===\n"
 make clean
 make
 
 ############################################################
-printf "\n=== TESTS WITHOUT ERROR ===\n"
+printf "\n\n=== TESTS WITHOUT ERROR === IF ($ ? == 0) AND (ERROR == 0) THEN TEST PASSED\n"
 
 counter=0
 for i in `ls $GOOD_dir`
 do
 	counter=$(expr $counter + 1)
-	printf "== $counter.) $i \n"
+	printf "= $counter.) $i \n"
     ./ifj $GOOD_dir/$i >>subor 2>&1
-    printf "\tECHO $ ? = $?\n"
+    exitvalue=$?
     error=$(cat subor | grep ^ERR.* | wc -l)
-    printf "\tERROR    = %s\n" "$error"
+    if [ $exitvalue -ne 0 ] && [ $error -ne 0 ]; then
+        printf "\tECHO $ ? = $exitvalue\n"
+        printf "\tERROR    = %s\n" "$error"
+    fi
 
     rm subor
 done
 
 ############################################################
-printf "\n=== TESTS WITH ERROR ===\n"
+printf "\n\n=== TESTS WITH ERROR === IF ($ ? != 0) AND (ERROR != 0) THEN TEST PASSED\n"
 
 counter=0
 for i in `ls $BAD_dir`
 do
 	counter=$(expr $counter + 1)
-	printf "== $counter.) $i \n"
+	printf "= $counter.) $i \n"
     ./ifj $BAD_dir/$i >>subor 2>&1
-    printf "\tECHO $ ? = $?\n"
+    exitvalue=$?
     error=$(cat subor | grep ^ERR.* | wc -l)
-    printf "\tERROR    = %s\n" "$error"
-
+    if [ $exitvalue -eq 0 ] || [ $error -eq 0 ]; then
+        printf "\tECHO $ ? = $exitvalue\n"
+        printf "\tERROR    = %s\n" "$error"
+    fi
     rm subor
 done
