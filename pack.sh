8b65efd7dda2488a5e50ab386eb80234f8fa2c50
Adam Suba <asuba512@gmail.com>
2016-11-27 23:48:07 +0100

    Packing script now adds header to all *.h and *.c files.

--- pack.sh
+++ pack.sh
@@ -7,6 +7,9 @@ xpalie00=25
 xsuhaj02=25
 xtotha01=25
 
+declare -A contributors
+declare -A tmpCon
+
 while getopts ":g" opt; do
   case $opt in
     g)
@@ -22,6 +25,47 @@ rm -rf *.o ifj sc_test sem_test prec_test .fuse*
 contents=`ls | grep -v -e "test" -e "docs" -e "pack" -e "README"`
 mkdir -p $tmpdir
 cp -r ${contents} $tmpdir
+
+for file in $contents
+do
+  if [ $file = "Makefile" ]; then
+    continue
+  fi
+  tmp=`git shortlog -s -n -- $file | tr -d " " | tr "\t" ":" | awk 'BEGIN{FS=":";OFS="="}{ t=$1; $1="["$2"]"; $2=t;print; }' | tr "\n" " "`
+  eval "tmpCon=($tmp)"
+  for contr in "${!tmpCon[@]}"
+  do
+      if [ $contr = "AdamSuba" ] && [ ${tmpCon[$contr]} -ge 4 ]
+      then
+        contributors[xsubaa00]=${tmpCon[$contr]}
+      fi
+      if ([ $contr = "xpalie00" ] || [ $contr = "xpalie42" ]) && [ ${tmpCon[$contr]} -ge 2 ]
+      then
+        contributors[xpalie00]=`expr ${contributors[xpalie00]} + ${tmpCon[$contr]}`
+      fi
+      if [ $contr = "peter2141" ] && [ ${tmpCon[$contr]} -ge 2 ]
+      then
+        contributors[xsuhaj02]=${tmpCon[$contr]}
+      fi
+      if ([ $contr = "AdriánTóth" ] || [ $contr = "europ" ]) && [ ${tmpCon[$contr]} -ge 2 ]
+      then
+        contributors[xtotha01]=`expr ${contributors[xtotha01]} + ${tmpCon[$contr]}`
+      fi
+  done
+
+  currentdir=`pwd`
+  cd $tmpdir
+
+  authors=`echo ${!contributors[@]} | tr " " "\n" | sort | tr "\n" " "`
+  authors=${authors:0:-1}
+  message="/***\n * IFJ16 programming language interpret\n * Contributors: ${authors// /, }\n***/\n\n"
+
+  sed -i '1s@^@'"$message"'@' $file
+
+  cd $currentdir
+  contributors=()
+  tmpCon=()
+done
 
 if [ $generate -eq 1 ]; then
     touch $tmpdir/dokumentace.pdf
